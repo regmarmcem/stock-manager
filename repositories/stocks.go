eb3da18975c5f449544c93e02878ef9df37d5e2a
regmarmcem <tknktknktknktknktknk@gmail.com>
2023-04-28 23:12:25 +0900

    [add] stock repositories

---
+++ repositories/stocks.go
@@ -0,0 +1,28 @@
+package repositories
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/regmarmcem/stock-manager/models"
+)
+
+func SelectStocks(db *sql.DB, stockID int) (models.Stock, error) {
+	const sqlStr = `
+		select id, name
+		from stocks
+		wheree stock_id = ?;
+	`
+	row := db.QueryRow(sqlStr, stockID)
+	if err := row.Err(); err != nil {
+		log.Println("SelectStocks failed")
+		return models.Stock{}, nil
+	}
+	var stock models.Stock
+	err := row.Scan(&stock.ID, &stock.Name)
+	if err != nil {
+		return models.Stock{}, err
+	}
+
+	return stock, nil
+}
