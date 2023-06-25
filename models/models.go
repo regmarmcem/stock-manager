2305dfe2719e2c5891d79dcbfcf6bf33188a77e3
regmarmcem <tknktknktknktknktknk@gmail.com>
2023-06-26 08:48:43 +0900

    [fix] models

--- models/models.go
+++ models/models.go
@@ -1,6 +1,15 @@
 package models
 
 type Stock struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	ImageID     string `json:"image_id"`
+}
+
+type Image struct {
+	ID           int    `json:"id"`
+	Title        string `json:"title"`
+	ImageURL     string `json:"image_url"`
+	ThumbnailURL string `json:"thumbnail_url"`
 }
